4688801c692e5b92d1805982f82a399bf451375f
Jesse <jdnely@gmail.com>
2021-09-02 09:52:21 -0700

    commit tree 73b5f7f651c39338627d19f67dcb7fb62463e723

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+// const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+// function returnFirstTwoDrivers() {
+//     const newDrivers = drivers.slice(0,2)
+//     return newDrivers;
+// }
+// function returnLastTwoDrivers(){
+//     const newDrivers = drivers.slice(2);
+//     return newDrivers;
+// }
+
+const returnFirstTwoDrivers = function(arrayOfDrivers) {['Sally', 'Bob', 'Freddy', 'Claudia']}
